f9d54546a8c10c526fb816262b1a7c8f31cbfc72
Vats1727 <vatsalparmar1742002@gmail.com>
2026-05-01 17:46:45 +0530

    Removed unwanted files

--- test_about_data_2.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'dev/config.php';
-try {
-    $db = new PDO("sqlite:" . PROJECT_DB_PATH);
-    $rows = $db->query("SELECT * FROM about_section")->fetchAll(PDO::FETCH_ASSOC);
-    print_r($rows);
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
--- test_fields.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'dev/config.php';
-try {
-    $db = new PDO("sqlite:" . DEV_DB_PATH);
-    $rows = $db->query("SELECT * FROM fields WHERE section_id IN (SELECT id FROM sections WHERE slug='service_list')")->fetchAll(PDO::FETCH_ASSOC);
-    print_r($rows);
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
